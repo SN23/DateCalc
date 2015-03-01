753131823f2aead69eabcfb4551d8f5ed037cf1b
Sukhjinder Nahal <nahal.snahal@gmail.com>
2015-03-01 16:53:36 -0500

    Implemented adding and subtracting of days

--- DateCalc/src/DateCalc.java
+++ DateCalc/src/DateCalc.java
@@ -12,12 +12,37 @@ public class DateCalc {
 			System.out.println("Todays date is " + today.format(DateTimeFormatter.ofPattern("MM/d/uuuu")));
 			int days;
 			
-			System.out.println("Enter (a) to add days, (b) to subtract days, (e) to exit ");
+			System.out.println("Enter (a) to add days, (s) to subtract days, (e) to exit ");
 			Scanner keyboard = new Scanner(System.in);
 			String input = keyboard.next();
 			
 			
+			if(input.equalsIgnoreCase("A"))
+			{
+				System.out.println("How many days do you want to add");
+				days = keyboard.nextInt();
+				if(days>0 & days<=31)
+				{
+					String newDate = today.plusDays(days).format(DateTimeFormatter.ofPattern("MM/d/uuuu"));
+					System.out.println(newDate);
+				}
+			}
+		
+			if(input.equalsIgnoreCase("s"))
+			{
+				System.out.println("How many days do you want to subtract");
+				days = keyboard.nextInt();
+				if(days>0 & days<=31)
+				{
+					String newDate = today.minusDays(days).format(DateTimeFormatter.ofPattern("MM/d/uuuu"));
+					System.out.println(newDate);
+				}
+			}
 			
+			if(input.equalsIgnoreCase("e"))
+			{
+				System.out.println("Bye");
+			}
 			
 			
 			
